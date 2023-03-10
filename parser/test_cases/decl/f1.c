e87a51444669367e1109e918aaa92ca5c7646d91
Gregory Presser <gpress2222@gmail.com>
2023-03-10 12:39:38 -0500

    added more tests

---
+++ parser/test_cases/decl/f1.c
@@ -0,0 +1,13 @@
+int f();
+int h(int a);
+
+int f(double i) { int j; }
+
+void (*g(double d))(int a, char b) {
+    int f();
+    int g();
+}
+
+int h(int x) { int h; }
+
+int kr(int i, int j, double k) {}
